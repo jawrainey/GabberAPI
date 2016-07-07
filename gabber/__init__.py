1b7f29252b9ca50af58e8ba1afc311b9b61f4b2a
Jay Rainey <jawrainey@gmail.com>
2016-07-07 13:52:26 +0100

    Put database with experiences for now
    This is required a persistent volume on dokku will exist for the
    experiences above root (where users cannot view). If not persistent,
    then it would be wiped with each commit, oh my.

--- gabber/__init__.py
+++ gabber/__init__.py
@@ -7,14 +7,19 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.debug = True
 # This is where all the audio experiences will be stored -- root directory.
-xp = os.path.abspath(os.path.join("experiences"))
+xp = os.path.abspath(os.path.join(os.pardir, "experiences"))
 if not os.path.exists(xp):
     os.makedirs(xp)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + 'dati.db'
+dbp = os.path.join(xp, 'dati.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbp
 app.config['SECRET_KEY'] = 'supersecretpasswordfromtheotherside'
 app.config['UPLOAD_FOLDER'] = xp
 
 db = SQLAlchemy(app)
 
-from gabber import views, models, api
+from gabber import models, api
+
+# Create the database afterwards as models meta-data required to populate it.
+if not os.path.exists(dbp):
+    db.create_all()
