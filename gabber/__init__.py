b3702d28f74d45730583c9efb4b17fd971d278c1
Jay Rainey <jawrainey@gmail.com>
2017-02-13 16:25:05 +0000

    Update existing projects through website
    - Added basic views for user dashboard & editing a project

--- gabber/__init__.py
+++ gabber/__init__.py
@@ -16,7 +16,9 @@ login_manager = LoginManager(app)
 
 app.debug = True
 # This is where all the audio interviews will be stored -- root directory.
-xp = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'protected')
+root = os.path.dirname(os.path.abspath(__file__))
+
+xp = os.path.join(root, 'protected')
 if not os.path.exists(xp):
     os.makedirs(xp)
 
@@ -26,6 +28,8 @@ app.config['SECRET_KEY'] = 'supersecretpasswordfromtheotherside'
 app.config['UPLOAD_FOLDER'] = xp
 app.config['SALT'] = 'supersecretsaltfromtheotherside'
 app.config['PROXY_PATH'] = PROXY_PATH
+app.config['IMG_FOLDER'] = os.path.join(root, 'static/img/')
+
 
 # Gmail for simplicity of initial deployment.
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -43,6 +47,8 @@ from gabber.views import main
 app.register_blueprint(main, url_prefix=PROXY_PATH)
 from gabber.api import api
 app.register_blueprint(api, url_prefix=os.path.join(PROXY_PATH, 'api/'))
+from gabber.users.views import users
+app.register_blueprint(users, url_prefix=PROXY_PATH)
 from gabber.users.views import users
 app.register_blueprint(users, url_prefix=PROXY_PATH)
 
--- gabber/users/views.py
+++ gabber/users/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask.ext.login import current_user, login_user, login_required, logout_user
 from gabber.users.forms import LoginForm
 from gabber.users.models import User
-from gabber import login_manager
+from gabber.projects.models import Project
+from gabber import app, db, login_manager
+import os
 
 users = Blueprint('users', __name__)
 
@@ -35,10 +37,46 @@ def logout():
 @users.route('dashboard/', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    pass
+    projects = Project.query.filter_by(creator=current_user.username).all()
+    return render_template('views/users/dashboard.html', projects=projects)
 
 
 @users.route('dashboard/edit/<project>/', methods=['GET', 'POST'])
 @login_required
 def edit_project(project=None):
-    pass
+    project = Project.query.filter_by(title=project).first()
+
+    # TODO: use WTForms to process and validate form. Tricky with dynamic form.
+    if request.method == 'POST':
+        # Allows title removal to create a 'prompt only' dictionary for parsing
+        _form = request.form.copy()
+        project.title = _form.get('title', None)
+        _form.pop('title')
+
+        prompts = project.prompts.all()
+
+        for fieldname, prompt_text in _form.iteritems():
+            __update_prompt(prompts, fieldname.split("-")[-1], text=prompt_text)
+
+        for fieldname, uploaded_file in request.files.iteritems():
+            if uploaded_file.filename:
+                folder = os.path.join(app.config['IMG_FOLDER'] + str(project.id))
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                fname = fieldname.split("-")[-1] + '.jpg'
+                uploaded_file.save(os.path.join(folder, fname))
+                __update_prompt(prompts, fname.split('.')[0], image=fname)
+
+        db.session.commit()
+        flash('The prompts for your project have been updated if any changes were made.')
+        return redirect(url_for('users.dashboard'))
+    return render_template('views/users/edit.html', project=project)
+
+
+def __update_prompt(prompts, prompt_id, text=None, image=None):
+    for prompt in prompts:
+        if int(prompt_id) == prompt.id:
+            if text:
+                prompt.text_prompt = text
+            if image:
+                prompt.image_path = image
