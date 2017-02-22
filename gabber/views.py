e1784c3b3bd5796b74bed3778659b156b95cf857
Jay Rainey <jawrainey@gmail.com>
2017-02-22 13:12:39 +0000

    Remove duplicate interviews from IDM view

--- gabber/views.py
+++ gabber/views.py
@@ -26,17 +26,26 @@ def projects():
 def idm():
     from sqlalchemy.sql import and_
     import datetime
-    prompt_ids = [i.id for i in Project.query.filter_by(id=1).first().prompts.all()]
+    prompt_ids = [i.id for i in
+                  Project.query.filter_by(id=1).first().prompts.all()]
 
     interviews = Interview.query.filter(
         and_(Interview.prompt_id.in_(prompt_ids),
              Interview.created_on >= datetime.datetime(2017,2,15,15,00,00))
     ).order_by(Interview.created_on.desc()).all()
 
-    interviews = [i for i in interviews for interview in i.participants.all()
-                  if 'ncl' in interview.email or 'newcastle' in interview.email]
+    print len(interviews)
+    filtered_interviews = []
+
+    for i in interviews:
+        print i.participants.all()
+        for j in i.participants.all():
+            if 'ncl' in j.email or 'newcastle' in j.email:
+                filtered_interviews.append(i)
+                break
+
     prompts = ProjectPrompt.query.filter(ProjectPrompt.id.in_(prompt_ids)).all()
     prompts = dict((p.id, p.text_prompt) for p in prompts)
     return render_template('views/main/idm.html',
-                           interviews=interviews,
+                           interviews=filtered_interviews,
                            prompts=prompts)
