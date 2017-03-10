28fa9cdcf0e2a609f9dd2643860bfe7647410651
Jay Rainey <jawrainey@gmail.com>
2017-03-10 13:54:27 +0000

    Show private projects to admins or staff
    - Who are associated with these projects

--- gabber/main/views.py
+++ gabber/main/views.py
@@ -1,5 +1,6 @@
 from gabber.projects.models import Project, ProjectPrompt, Interview
 from flask import Blueprint, redirect, render_template, flash, url_for
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
@@ -16,8 +17,14 @@ def about():
 
 @main.route('projects/', methods=['GET'])
 def projects():
-    flash('No projects are currently public for you to listen to or curate.')
-    return redirect(url_for('main.about'))
+    public_projects = Project.query.filter_by(type=1).all()
+    user_projects = []
+    if current_user.is_authenticated and (current_user.get_role() == 'admin' or current_user.get_role() == 'staff'):
+        # Projects the user is a member of (whether private & public)
+        user_projects = current_user.projects
+        # Do not show the same projects in the public section if you are a member of that project
+        public_projects = list(set(public_projects) - set(user_projects))
+    return render_template('views/main/projects.html', user_projects=user_projects, public_projects=public_projects)
 
 
 @main.route('idm/', methods=['GET'])
