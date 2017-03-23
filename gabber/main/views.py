ef0bdea0c80ff38ea550186aa0ae0aacb6e0846f
Jay Rainey <jawrainey@gmail.com>
2017-03-23 11:28:56 +0000

    Remove IDM view as students can use project.display instead

--- gabber/main/views.py
+++ gabber/main/views.py
@@ -25,30 +25,3 @@ def projects():
         # Do not show the same projects in the public section if you are a member of that project
         public_projects = list(set(public_projects) - set(user_projects))
     return render_template('views/main/projects.html', user_projects=user_projects, public_projects=public_projects)
-
-
-@main.route('idm/', methods=['GET'])
-def idm():
-    from sqlalchemy.sql import and_
-    import datetime
-    prompt_ids = [i.id for i in
-                  Project.query.filter_by(id=1).first().prompts.all()]
-
-    interviews = Interview.query.filter(
-        and_(Interview.prompt_id.in_(prompt_ids),
-             Interview.created_on >= datetime.datetime(2017, 2, 15, 15, 00, 00))
-    ).order_by(Interview.created_on.desc()).all()
-
-    filtered_interviews = []
-
-    for i in interviews:
-        for j in i.participants.all():
-            if 'ncl' in j.email or 'newcastle' in j.email:
-                filtered_interviews.append(i)
-                break
-
-    prompts = ProjectPrompt.query.filter(ProjectPrompt.id.in_(prompt_ids)).all()
-    prompts = dict((p.id, p.text_prompt) for p in prompts)
-    return render_template('views/main/idm.html',
-                           interviews=filtered_interviews,
-                           prompts=prompts)
