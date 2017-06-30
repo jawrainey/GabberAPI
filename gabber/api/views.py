f7f9d3f478f4a89444661bdcd52efc8d54e57917
Jay Rainey <jawrainey@gmail.com>
2017-06-30 10:00:54 +0100

    Allow an admin to add members to their project
    - Added a `card-reveal` to allow a user to add a member directly to a
    project from within the /projects/ page
    - Modified `render_projects` to add a lock icon to illustrate
    public/private type of the project.
    - Use a form within card-reveal for input validation when button
    pressed; this could be removed and javascript used in the future
    instead.

--- gabber/api/views.py
+++ gabber/api/views.py
@@ -233,3 +233,56 @@ def create_connection():
     db.session.commit()
 
     return jsonify({'success': True}), 200
+
+
+@api.route('member/add/', methods=['POST'])
+def add_member():
+    """
+    Allows a user with an admin role to add a registered user to their project.
+
+    Args:
+        json:
+            uid (int): the user id of the individual making this request
+            pid (int): the project the admin wants to add a member to
+            email (str): the email address of the registered user to add
+
+    Returns:
+        json: 'success' if the prompt was deleted or 'error' and related message.
+    """
+    from gabber.projects.models import Membership, Project, Roles
+
+    req = request.get_json()
+
+    uid = req.get('uid', None)
+    pid = req.get('pid', None)
+    email = req.get('email', None)
+
+    # The user must exist before it can become a project member
+    if email and email not in [user.username for user in db.session.query(User.username)]:
+        return jsonify({'error': 'This email (%s) is not linked to a registered account.' % email}), 400
+
+    # The user must be an admin of this particular project
+    role_for_this_project = User.query.get(uid).member_of.filter_by(project_id=pid).first().role_id
+    if role_for_this_project is not Roles.query.filter_by(name='admin').first().id:
+        return jsonify({'error': 'You must be an admin of a project to add members to it.'}), 400
+
+    # The project must exist before we try to get it
+    project = Project.query.get(pid)
+    if not project:
+        return jsonify({'error': 'No project exists with that ID'}), 400
+
+    id_of_member_to_add = User.query.filter_by(username=email).first().id
+
+    # A user cannot be a member of the project more than once
+    if id_of_member_to_add and id_of_member_to_add in [m.user_id for m in project.members]:
+        return jsonify({'error': 'A user with that email is already a member of %s' % project.title}), 400
+
+    # Make this registered user a member of this project
+    user_role = Roles.query.filter_by(name='user').first().id
+    membership = Membership(uid=id_of_member_to_add, pid=pid, rid=user_role)
+    project.members.append(membership)
+
+    db.session.add(project)
+    db.session.commit()
+
+    return jsonify({'success': True}), 200
