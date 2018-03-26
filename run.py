82299271c3f89da4500c8a31a9de21e07c010969
Jay Rainey <jawrainey@gmail.com>
2018-03-26 10:21:47 +0100

    :bug: Factory must be created outside of main
    Also forgot to update docker compose when I renamed env variables

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from gabber import create_app
 import os
 
+app = create_app(os.environ.get('APP_MODE', 'dev'))
+
 if __name__ == '__main__':
-    app = create_app(os.environ.get('APP_MODE', 'dev'))
     app.run()
