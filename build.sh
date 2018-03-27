b7bc17d45f0f5b72f9e93d41463c088e24cda940
Jay Rainey <jawrainey@gmail.com>
2018-03-27 15:27:59 +0100

    :wrench: Added build script to publish docker build to registry

---
+++ build.sh
@@ -0,0 +1,12 @@
+if [ -z "$1" ]
+then
+    echo "Pass version, e.g x.y.z"
+    exit 1
+fi
+
+REGISTRY="openlab.ncl.ac.uk:4567/gabber/api"
+VERSION="$1"
+
+docker build -t "$REGISTRY:$VERSION" -t "$REGISTRY:latest" .
+docker push "$REGISTRY:$VERSION"
+docker push "$REGISTRY:latest"
